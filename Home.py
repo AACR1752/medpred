ec18ad142d56cbc5033928bf0f2237a1c427b439
AACR1752 <aa5chowd@uwaterloo.ca>
2025-03-08 14:35:35 -0500

    push changes

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
     # Set page configuration
 st.set_page_config(
@@ -12,6 +13,23 @@ st.title("Welcome to My Streamlit App")
 st.write("This is a multi-page Streamlit application template.")
     
 
+df = pd.read_csv('data/med_inv_dataset.csv')
+df.columns = df.columns.str.lower()
+    
+# st.selectbox('Select a column', df['drugname'].unique())
+selected_drugname = st.selectbox('Select a column', df['drugname'].unique())
+
+if st.button("Predict"):
+    subcat_value = df.loc[df['drugname'] == selected_drugname, 'subcat'].iloc[0]
+    st.write(f"Sub Cat: {subcat_value}")
+
+
+    
+    
+    
+    
+    
+    
     
     # Sidebar (still useful for page-specific controls)
 with st.sidebar:
